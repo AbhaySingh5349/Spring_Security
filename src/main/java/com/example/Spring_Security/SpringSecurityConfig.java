dd4b9d512308331864018f738ce4450dad28ed30
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-07-15 14:11:13 +0530

    InMemory authentication and authorization

--- src/main/java/com/example/Spring_Security/SpringSecurityConfig.java
+++ src/main/java/com/example/Spring_Security/SpringSecurityConfig.java
@@ -2,13 +2,18 @@ package com.example.Spring_Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 // whenever we want to create our own beans, we need '@Configuration' annotation
+// http://localhost:8080/spring-security
 
 @Configuration
 public class SpringSecurityConfig {
@@ -27,8 +32,25 @@ public class SpringSecurityConfig {
         return new InMemoryUserDetailsManager(user, admin);
     }
 
+    // to specify which API can be accessed different authorities (authorization for routes)
+    // API should have at least 1 of authorities assigned to users
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/home/**").permitAll() // even if user is un-authenticated, access is allowed
+                        .requestMatchers("/admin").hasAuthority("ADMIN")
+                        .requestMatchers("/user").hasAuthority("USER")
+                        .requestMatchers("/spring-security").hasAnyAuthority("ADMIN", "USER")
+                        .anyRequest().permitAll())
+                .formLogin(withDefaults()) // we need a login form for accessing authorized routes (browser)
+                .httpBasic(withDefaults()); // for postman, we need to provide "Basic Auth"
+        return http.build();
+    }
+
+    // password encoding
     @Bean
     public PasswordEncoder getEncoder(){
         return NoOpPasswordEncoder.getInstance(); // no encryption is done for user & password, plain text comparison
     }
+
 }
--- src/main/java/com/example/Spring_Security/controller/SecurityController.java
+++ src/main/java/com/example/Spring_Security/controller/SecurityController.java
@@ -7,6 +7,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
     @GetMapping("/spring-security")
     public String springSecurity(){
-        return "Spring Security";
+        return "Spring Security Route";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "Home Route";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "User Route";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Admin Route";
     }
 }
